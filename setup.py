815e965fadd49e75e6dd7b9d005c5906ba6f59f9
Andrew Martin <a.a.d.martin@gmail.com>
2014-11-08 00:01:20 +1100

    added dependency management through setuptools

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='EnvComparison',
-      version='0.1.4',
+      version='0.1.5',
       description='Linux Environment Comparison Tool',
       author='Andrew Martin',
       author_email='a.a.d.martin@gmail.com',
       url='https://github.com/adamar/EnvComparison',
-      scripts=["compare.py"],
-      packages=['EnvComparison']
+      scripts=['compare.py'],
+      packages=['EnvComparison'],
+      install_requires=['paramiko','tornado']
      )
