7799621c88d0b512beb93d491baf415d3cbe2c3d
Andrew Martin <a.a.d.martin@gmail.com>
2014-11-07 23:36:32 +1100

    moved files to fix import

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='EnvComparison',
-      version='0.1',
+      version='0.1.3',
       description='Linux Environment Comparison Tool',
       author='Andrew Martin',
       author_email='a.a.d.martin@gmail.com',
