88e345d0773c8aa4e990ea3a38047158f4e1bfec
Andrew Martin <a.a.d.martin@gmail.com>
2014-11-07 22:23:55 +1100

    Added compare scripts

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(name='EnvComparison',
       author='Andrew Martin',
       author_email='a.a.d.martin@gmail.com',
       url='https://github.com/adamar/EnvComparison',
+      scripts=["compare.py"]
       install_requires=["paramiko", "tornado"],
       packages=['EnvComparison'],
      )
